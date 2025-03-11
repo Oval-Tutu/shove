e7436eba3b939cdc5216bd2a24ad016db50a2f6d
Martin Wimpress <martin@wimpress.org>
2025-03-11 20:26:25 +0000

    style: format all .lua files with Stylua

--- demo/main.lua
+++ demo/main.lua
@@ -1,35 +1,36 @@
-shove = require("shove") --require the library
-love.window.setTitle("Press space to switch examples!")
+shove = require("shove")
+love.window.setTitle("Press space to switch demo!")
 
 local examples = {
-	"low-res",
-	"single-shader",
-	"multiple-shaders",
-	"mouse-input",
-	"canvases-shaders",
-	"stencil"
+  "low-res",
+  "single-shader",
+  "multiple-shaders",
+  "mouse-input",
+  "canvases-shaders",
+  "stencil",
 }
 local example = 1
 
 for i = 1, #examples do
-	examples[i] = require(examples[i])
-end
-
-function love.resize(w, h)
-	shove.resize(w, h)
-end
-
-function love.keypressed(key)
-	if key == "space" then
-		example = (example < #examples) and example + 1 or 1
-		examples[example]()
-		love.load()
-	elseif key == "f" then -- Activate fullscreen mode
-		love.window.setMode(0, 0, { fullscreen = true, fullscreentype = "desktop" })
-		shove.resize(love.graphics.getDimensions())
-	elseif key == "escape" then
-		love.event.quit()
-	end
+  examples[i] = require(examples[i])
 end
 
 examples[example]()
+
+function love.resize(w, h)
+  shove.resize(w, h)
+end
+
+function love.keypressed(key)
+  if key == "space" then
+    example = (example < #examples) and example + 1 or 1
+    examples[example]()
+    love.load()
+  elseif key == "f" then
+    -- Activate fullscreen mode
+    love.window.setMode(0, 0, { fullscreen = true, fullscreentype = "desktop" })
+    shove.resize(love.graphics.getDimensions())
+  elseif key == "escape" and love.system.getOS() ~= "Web" then
+    love.event.quit()
+  end
+end
--- demo/mouse-input/init.lua
+++ demo/mouse-input/init.lua
@@ -1,35 +1,38 @@
---[[ Mouse input ]]--
+return function()
+  love.graphics.setDefaultFilter("linear", "linear")
+  love.window.setMode(1280, 720, { resizable = true })
+  shove.setupScreen(800, 600, { canvas = true })
 
-return function ()
+  function love.load()
+    love.mouse.setVisible(false)
+    love.graphics.setNewFont(32)
+  end
 
-	love.graphics.setDefaultFilter("linear", "linear") -- Bilinear scaling
+  function love.draw()
+    local shoveWidth, shoveHeight = shove.getDimensions()
 
-	love.window.setMode(1280, 720, {resizable = true}) -- LÖVE resolution 1280x720, resizable
-	shove.setupScreen(800, 600, {canvas = true})
+    shove.start()
+      love.graphics.setColor(50, 0, 0)
+      love.graphics.rectangle("fill", 0, 0, shoveWidth, shoveHeight)
 
-	function love.load()
-		love.mouse.setVisible(false)
-		love.graphics.setNewFont(32)
-	end
+      local mouseX, mouseY = love.mouse.getPosition()
+      -- false is returned if mouse is outside the game screen
+      mouseX, mouseY = shove.toGame(mouseX, mouseY)
+      -- Good practice to floor returned values when simulating screen pixels
+      if mouseX then
+        mouseX = math.floor(mouseX)
+      end
+      if mouseY then
+        mouseY = math.floor(mouseY)
+      end
 
-	function love.draw()
-		local shoveWidth, shoveHeight = shove.getDimensions()
+      love.graphics.setColor(255, 255, 255)
+      if mouseX and mouseY then
+        love.graphics.circle("line", mouseX, mouseY, 10)
+      end
 
-		shove.start()
-			love.graphics.setColor(50, 0, 0)
-			love.graphics.rectangle("fill", 0, 0, shoveWidth, shoveHeight)
-
-			local mouseX, mouseY = love.mouse.getPosition()
-			mouseX, mouseY = shove.toGame(mouseX, mouseY) -- false is returned if mouse is outside the game screen
-			-- Good practice to floor returned values when simulating screen pixels
-			if mouseX then mouseX = math.floor(mouseX) end
-			if mouseY then mouseY = math.floor(mouseY) end
-
-			love.graphics.setColor(255, 255, 255)
-			if mouseX and mouseY then love.graphics.circle("line", mouseX, mouseY, 10) end
-
-			love.graphics.printf("mouse x : " .. (mouseX or "outside"), 25, 25, shoveWidth, "left")
-			love.graphics.printf("mouse y : " .. (mouseY or "outside"), 25, 50, shoveWidth, "left")
-		shove.finish()
-	end
+      love.graphics.printf("mouse x : " .. (mouseX or "outside"), 25, 25, shoveWidth, "left")
+      love.graphics.printf("mouse y : " .. (mouseY or "outside"), 25, 50, shoveWidth, "left")
+    shove.finish()
+  end
 end
